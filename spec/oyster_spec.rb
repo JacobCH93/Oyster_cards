25e78f9c19787094385c6f1d7474bdc103dc0deb
Jacob <jacobchanrahan@gmail.com>
2021-10-25 16:30:50 +0100

    Adding a balance test and method

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -5,7 +5,12 @@ describe OysterCard do
     end
 
     it "checks balance in card" do
-        oystercard = OysterCard.new
-        expect(oystercard.balance).to eq (0)
+        expect(subject.balance).to eq (0)
     end 
+    describe '#top_up' do
+        it { is_expected.to respond_to(:top_up).with(1).argument }
+    end
+        it 'can top up the balance' do
+        expect{subject.top_up (1) }.to change{ subject.balance }.by (1)
+    end
 end
