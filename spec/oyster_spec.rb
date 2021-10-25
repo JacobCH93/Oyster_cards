7ca424d91e32c62114704c850e76c45f9fd15288
roshanabey2 <roshanabey2@gmail.com>
2021-10-25 15:41:44 +0100

    created Oystercard.rb file and created class with the attribute balance that initializes the value as 0. Also created correspond tests fir it

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,2 +1,11 @@
+require 'OysterCard'
 describe OysterCard do
+    it "it doesn't give an error" do
+        expect{ oystercard = OysterCard.new }.not_to raise_error
+    end
+
+    it "checks balance in card" do
+        oystercard = OysterCard.new
+        expect(oystercard.balance).to eq (0)
+    end 
 end
